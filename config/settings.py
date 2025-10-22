c35ced37e43cc9491b761970b7d3a8bd7f6e47a1
yamuna <yamuna.pdy@gmail.com>
2025-10-22 22:32:03 +0300

    updated

--- config/settings.py
+++ config/settings.py
@@ -139,8 +139,20 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+cloudinary.config(
+    cloud_name="dzkfswtru",
+    api_key="292984143432322",
+    api_secret="**********************",
+    secure=True
+)
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': ['django_filters.rest_framework.DjangoFilterBackend']
+}
